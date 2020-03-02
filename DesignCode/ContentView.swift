d56e42d027ffaa3aaef253d137758fe6376d8eb3
bluecode <1039755466@qq.com>
2020-03-02 16:08:46 +0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by 周会勤 on 2020/3/2.
+//  Copyright © 2020 周会勤. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
