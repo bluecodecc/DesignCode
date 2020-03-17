c84391f478031ef97875f5029e1323b7890d5d39
bluecode <1039755466@qq.com>
2020-03-17 16:23:56 +0800

    颜色渐变,页面覆盖

--- DesignCode/MenuView.swift
+++ DesignCode/MenuView.swift
@@ -13,20 +13,44 @@ struct MenuView: View {
         
         VStack {
             Spacer()
-            VStack {
+            VStack(spacing: 20.0) {
+                Text("Qin - 99% complete")
+               .font(.caption)
+           
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.0776166524)))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    .cornerRadius(12)
+                    .shadow(color: Color.white, radius: 0, x: 0, y: 1)
+                
                 MenuRow(icon: "gear", title: "Account")
                 MenuRow(icon: "creditcard", title: "Billing")
                 MenuRow(icon: "person.crop.circle",title: "Signed Out")
             }
             .frame(height: 300.0)
             .frame(maxWidth: .infinity)
-            .background(Color.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.871, green: 0.894, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-             .shadow(radius: 30)
+            .shadow(radius: 30)
             .padding(.horizontal, 30)
+            .overlay(
+                Image("Avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60.0, height: 60.0)
+                    .clipShape(Circle())
+                    .offset(y: -150)
+            )
+            
         }
         .padding(.bottom, 30)
-        
+        .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
         
     }
 }
@@ -44,6 +68,7 @@ struct MenuRow: View {
         HStack(spacing: 16.0) {
             Image(systemName: icon)
                 .font(.system(size: 20, weight: .bold))
+                .imageScale(.large)
             Text(title)
                 .font(.system(size: 20, weight: .bold, design: .rounded))
                 .frame(width: 120.0, alignment: .leading)
