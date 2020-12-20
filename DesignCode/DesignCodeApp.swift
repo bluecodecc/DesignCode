af832ba63ece23e75cd4f6053e0905826abc1bc8
bluecodecc <1039755466@qq.com>
2020-12-21 00:27:20 +0800

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by 周会勤 on 2020/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
