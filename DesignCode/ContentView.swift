4ec08a732aff10bedf9a1ae69ef1cfc6413459da
bluecodecc <1039755466@qq.com>
2020-12-21 22:20:55 +0800

    Layout and Stacks fix

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,48 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            HStack {
-                VStack {
-                    HStack(alignment: .top) {
-                        VStack(alignment: .leading) {
-                            Text("UI Design")
-                                .font(.system(size: 20))
-                                .fontWeight(.semibold)
-                            
-                            Text("Certificate")
-                                .foregroundColor(Color("accent"))
-                                .font(.system(size: 20))
-                        }
-                        
-                        Spacer()
-                        
-                        Image("Logo1")
-                    }.padding(20)
-                    
-                    Spacer()
-                    
-                    Image("Card1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 300, height: 110, alignment: .top)
-                }
-                .frame(width: 340, height: 220)
-                .background(Color.black)
-                .cornerRadius(20)
-                .shadow(radius: 20)
+            VStack{
+                Spacer()
             }
+            .frame(width: 300.0, height: 220.0)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
             
-            Color.blue
-                .frame(width: 300, height: 220)
-                .cornerRadius(20)
-                .shadow(radius: 20)
-                .offset(x: 0, y: -20)
-            Color.blue
-                .frame(width: 300, height: 220)
-                .cornerRadius(20)
-                .shadow(radius: 20)
-                .offset(x: 0, y: 20)
+            VStack {
+                HStack() {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding(20)
+                Spacer()
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300.0, height: 110.0)
+            }
+            .frame(width: 340, height: 220,alignment: .top)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
         }
+        
     }
     
 }
