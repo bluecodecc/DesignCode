ab3e6ff1cc4d2349453b2f3672d0382c88076177
bluecodecc <1039755466@qq.com>
2020-12-23 00:17:00 +0800

    Gestures and Events

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var show = false
-    
+    @State var viewState = CGSize.zero
     var body: some View {
         ZStack {
             TitleView()
@@ -20,6 +20,7 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.9)
                 .rotationEffect(.degrees(show ? 0 : 10))
                 .rotation3DEffect(
@@ -32,6 +33,7 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
                 .rotationEffect(.degrees(show ? 0 : 5))
                 .rotation3DEffect(
@@ -39,18 +41,25 @@ struct ContentView: View {
                     axis: (x:10, y: 0.0, z: 0.0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.3))
-
+            
             CardView()
+                .offset(x: viewState.width, y: viewState.height)
                 .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
                 .onTapGesture {
                     show.toggle()
                 }
-            
-            
-            BottomCardView()
-                .blur(radius: show ? 20  : 0)
-                .animation(.default)
-            
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            self.viewState = value.translation
+                            self.show = true
+                        })
+                        .onEnded({ value in
+                            self.viewState = .zero
+                            self.show = false
+                        })
+                )
         }
     }
 }
